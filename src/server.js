af6991972f857103b0167c2cbe527b6b1eebd142
whynot <aminfury@mail.ru>
2024-04-01 15:22:22 +0300

    init

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const PORT = 5000
+
+app.get('/', (req, res) => {
+    res.json({ message: 'main' })
+})
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
